bd8aa5979e69d60724b57827ee9fa02deccf4d3e
unknown <nghiait06@gmail.com>
2017-02-18 18:42:16 +0700

    delete folder

--- TmTech v1/TmTech v1/Model/PaymentMethod.cs
+++
@@ -1,9 +0,0 @@
-namespace TmTech_v1.Model
-{
-    public class PaymentMethod
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Note { get; set; }
-    }
-}
