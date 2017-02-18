56d175b28c9bda574292bcd79fe89138f65f120f
unknown <nghiait06@gmail.com>
2017-02-18 18:48:08 +0700

    delete of folder

--- TmTech v1/TmTech v1/Model/CustomerType.cs
+++
@@ -1,9 +0,0 @@
-
-namespace TmTech_v1.Model
-{
-    public class CustomerType
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- TmTech v1/TmTech v1/Model/ProductColor.cs
+++
@@ -1,9 +0,0 @@
-namespace TmTech_v1.Model
-{
-    public class ProductColor
-    {
-        public int ProductId { get; set; }
-        public int ColorId { get; set; }
-        public string Note { get; set; }
-    }
-}
